e0bd76cde6ba4a9805e737a987c3f4d9d160c932
Seth Achten <75361355+SethAchtenPXL@users.noreply.github.com>
2025-03-27 16:17:40 +0100

    opzetten front-end en homepage

---
+++ Frontend/gvpp/src/components/HeaderComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <header class="flex justify-between items-center p-4 top-0 left-0 right-0 z-10 bg-slate-200">
+      <div class="flex items-center mt-2">
+        <img src="/assets/placeholder-logo.png" alt="Logo" class="h-25 w-auto" />
+      </div>
+    </header>
+  </template>
+  <script setup lang="ts">
+  </script>

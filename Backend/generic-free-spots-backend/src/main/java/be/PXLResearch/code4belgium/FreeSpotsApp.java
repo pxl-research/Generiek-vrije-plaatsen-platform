eee784f41d6e04cfdc64247c144188bee06a0b33
SimonIvenPXL <simon.iven@student.pxl.be>
2025-04-02 12:00:33 +0200

    Created a Springboot backend for the generic free spots application

---
+++ Backend/generic-free-spots-backend/src/main/java/be/PXLResearch/code4belgium/FreeSpotsApp.java
@@ -0,0 +1,13 @@
+package be.PXLResearch.code4belgium;
+
+/**
+ * Hello world!
+ *
+ */
+public class FreeSpotsApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
